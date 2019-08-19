bb949b12fc2567e441ece5413a24fdb72499d183
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-08-19 19:08:47 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AHBC_2019_Midterm_JulyBC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
