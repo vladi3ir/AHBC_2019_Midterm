1adac6f827a44196138e8dfd991300729f65a79d
edwardsosnoski <edward.sosnoski@gmail.com>
2019-08-19 21:03:54 -0400

    Added a validator class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace AHBC_2019_Midterm_JulyBC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! modified");
+            Book eastOfEden = new Book("East of Eden", "John Steinbeck", true, new DateTime(2019, 10, 19));
+            Validator validator = new Validator();
+            Console.WriteLine($"Can you return {eastOfEden.Title}? {validator.IsReturnable(eastOfEden)}.");
         }
     }
 }
---
+++ Validator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AHBC_2019_Midterm_JulyBC
+{
+    public class Validator
+    {
+        public bool IsSearchType(string userInput, out string result)
+        {
+            result = userInput;
+
+            return (userInput.Contains("Title", StringComparison.OrdinalIgnoreCase) ||
+                userInput.Contains("Keyword", StringComparison.OrdinalIgnoreCase) ||
+                userInput.Equals("Author", StringComparison.OrdinalIgnoreCase));
+        }
+
+        public bool IsReturnable(Book userChoice)
+        {
+            return userChoice.IsCheckedOut;
+        }
+    }
+}
