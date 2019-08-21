1428cbeded7b2730e6978981a62850295bd01c80
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-08-21 18:49:49 -0400

    Adding search.cs

---
+++ Search.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AHBC_2019_Midterm_JulyBC
+{
+    class Search
+    {
+        public string _list { get; set; }
+        public Search(string list)
+        {
+            _list = list;
+
+        }
+        //public static List<string> SearchList(string userSearchInput, List<string> author, List<string>title, List<string> keyword)
+        //{
+        //    List<string> tempList = new List<string>();
+
+        //    //Console.WriteLine("Search by Title, Author, or keyword");
+        //    //string userInput = Console.ReadLine();
+
+        //    foreach (var item in author)
+        //    {
+        //        author.Contains(userSearchInput);
+        //        title.Contains(userSearchInput);
+        //    }
+
+        //    return tempList;
+        //}
+
+        public List<Book> SearchByTitle(string userInput, List<Book> listOfBooks)
+        {
+            List<Book> searchResults = new List<Book>();
+            foreach (Book book in listOfBooks)
+            {
+                if (book.Title.Contains(userInput, StringComparison.OrdinalIgnoreCase))
+                {
+                    searchResults.Add(book);
+                }
+            }
+
+            return searchResults;
+        }
+    }
+}
