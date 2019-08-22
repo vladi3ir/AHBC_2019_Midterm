0fb537e5dc243da24ab2071898a96eec980d3c19
Olaposi <ooomishope@yahoo.com>
2019-08-21 20:45:36 -0400

    wtf

--- BookList.cs
+++ BookList.cs
@@ -7,20 +7,20 @@ namespace AHBC_2019_Midterm_JulyBC
     public class BookList
     {
 
-        List<Books> books = new List<Books>
+        List<Book> books = new List<Book>
         {
-            new Books("20 Love Poems and a Song of Despair", "Pablo Neruda", true, new DateTime()),
-            new Books("1984", "Geogre Orwell", false, new DateTime(2019,9,30)),
-            new Books("The Art of War", "Sun Tsu", false, new DateTime(2019,8,25)),
-            new Books("48 Laws of Power", "Robert Greene", true, new DateTime()),
-            new Books("Moby Dick", "Herman Melville", true,new DateTime()),
-            new Books("War and Peace","Leo Tolstoy", false, new DateTime(2019,8,28)),
-            new Books("Hamlet", "william Shakespare", true, new DateTime()),
-            new Books("Heart of Darkness", "Joseph Conrad",true, new DateTime()),
-            new Books("Invisible Man", "Ralah Ellison", false, new DateTime(2019,12,1)),
-            new Books("To Kill a Mockingbird", "Harper Lee", true, new DateTime()),
-            new Books("As I Lay Dying", "William Faulkner", true, new DateTime()),
-            new Books("The Lord of the Rings", "J.R.R. Tolkien", true, new DateTime())
+            new Book("20 Love Poems and a Song of Despair", "Pablo Neruda", true, new DateTime()),
+            new Book("1984", "Geogre Orwell", false, new DateTime(2019,9,30)),
+            new Book("The Art of War", "Sun Tsu", false, new DateTime(2019,8,25)),
+            new Book("48 Laws of Power", "Robert Greene", true, new DateTime()),
+            new Book("Moby Dick", "Herman Melville", true,new DateTime()),
+            new Book("War and Peace","Leo Tolstoy", false, new DateTime(2019,8,28)),
+            new Book("Hamlet", "william Shakespare", true, new DateTime()),
+            new Book("Heart of Darkness", "Joseph Conrad",true, new DateTime()),
+            new Book("Invisible Man", "Ralah Ellison", false, new DateTime(2019,12,1)),
+            new Book("To Kill a Mockingbird", "Harper Lee", true, new DateTime()),
+            new Book("As I Lay Dying", "William Faulkner", true, new DateTime()),
+            new Book("The Lord of the Rings", "J.R.R. Tolkien", true, new DateTime())
         };
 
         
--- Search.cs
+++ Search.cs
@@ -30,8 +30,9 @@ namespace AHBC_2019_Midterm_JulyBC
                 }
             }
 
-        //        return searchResults;
-        //    }
-        //}
+            return searchResults;
+        }
     }
 }
+    
+
