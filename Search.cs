e0da08b2d4c4d0758bee76355d075c0cf68f75a6
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-08-21 20:27:36 -0400

    updated to search be author

--- Search.cs
+++ Search.cs
@@ -5,34 +5,26 @@ namespace AHBC_2019_Midterm_JulyBC
 {
     class Search
     {
-        public string _list { get; set; }
-        public Search(string list)
-        {
-            _list = list;
-
-        }
-        ////public static List<string> SearchList(string userSearchInput, List<string> author, List<string>title, List<string> keyword)
-        ////{
-        ////    List<string> tempList = new List<string>();
-
-        ////    //Console.WriteLine("Search by Title, Author, or keyword");
-        ////    //string userInput = Console.ReadLine();
-
-        ////    foreach (var item in author)
-        ////    {
-        ////        author.Contains(userSearchInput);
-        ////        title.Contains(userSearchInput);
-        ////    }
-
-        ////    return tempList;
-        ////}
-
         public List<Book> SearchByTitle(string userInput, List<Book> listOfBooks)
         {
             List<Book> searchResults = new List<Book>();
             foreach (Book book in listOfBooks)
             {
                 if (book.Title.Contains(userInput, StringComparison.OrdinalIgnoreCase))
+                {
+                    searchResults.Add(book);
+                }
+            }
+
+            return searchResults;
+        }
+
+        public List<Book> SearchByAuthor(string userInput, List<Book> listOfBooks)
+        {
+            List<Book> searchResults = new List<Book>();
+            foreach (Book book in listOfBooks)
+            {
+                if (book.Author.Contains(userInput, StringComparison.OrdinalIgnoreCase))
                 {
                     searchResults.Add(book);
                 }
