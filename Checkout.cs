2600d8425130498176156acb02b43cca4039eae2
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-08-22 21:41:15 -0400

    ADDED CHECKOUT METHOD CLASS

---
+++ Checkout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AHBC_2019_Midterm_JulyBC
+{
+    public class Checkout
+    {
+
+        public static void Checkout(List<Book> cart)
+        {
+            foreach (var item in cart)
+            {
+                item.IsCheckedOut = true;
+                item.ReturnDate = DateTime.Now.AddDays(14);
+            }
+        }
+    }
+}
+
