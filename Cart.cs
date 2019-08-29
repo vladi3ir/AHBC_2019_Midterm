b90ac989a68d4d25cee15d6f6bd99020fac035ea
Olaposi <ooomishope@yahoo.com>
2019-08-28 20:54:39 -0400

    The latest cart

--- Cart.cs
+++ Cart.cs
@@ -61,8 +61,10 @@ namespace AHBC_2019_Midterm_JulyBC
                     }
                     else
                     {
+                        Console.Clear();
                         Console.WriteLine("That is not a valid entry. Please press enter to try again.");
                         Console.ReadLine();
+                        Console.Clear();
                         validInput = false;
                     }
                 } while (!validInput);
